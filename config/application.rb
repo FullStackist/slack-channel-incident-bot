ac90e4e2738b59e802211598ff66e5c31ff900fd
onionsAbound <sean.pinilla.mcgovern@gmail.com>
2025-03-12 00:41:46 -0400

    ** Deploy V4 ** fixing asset pipeline #1

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module SlackChannelIncidentBot
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.assets.js_compressor = :uglifier
+    config.assets.compile = true
+
   end
 end
